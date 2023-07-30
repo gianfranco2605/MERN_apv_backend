ef272c8be530a3ed3b2c1875fd9feb63f70a5d2a
Gianfranco Navas Fernandini <gianf2605@gmail.com>
2023-07-31 01:00:54 +0200

    token auth ok

--- controllers/veterinarioController.js
+++ controllers/veterinarioController.js
@@ -24,9 +24,32 @@ const perfil = (req, res) => {
     res.json({
         msg: "Mostrando perfil"
     });
+};
+
+const confirmar = async (req, res) => {
+    const { token } = req.params;
+
+    const usuarioConfirmar = await Veterinario.findOne({token});
+
+    if( !usuarioConfirmar ) {
+        const error = new Error("Token no valido")
+        return res.status(404).json({ msg:error.message })
+    }
+    
+    try {
+        usuarioConfirmar.token = null;
+        usuarioConfirmar.confirmado = true;
+        await usuarioConfirmar.save();
+        res.json({msg: "Usuario confirmado correctamente"})
+    } catch (error) {
+        console.log(error);
+    }
+
+    
 }
 
 export {
     registrar,
-    perfil
+    perfil,
+    confirmar
 }
--- routes/veterinarioRoutes.js
+++ routes/veterinarioRoutes.js
@@ -1,11 +1,13 @@
 import express  from "express";
-import { registrar, perfil } from '../controllers/veterinarioController.js'
+import { registrar, perfil, confirmar } from '../controllers/veterinarioController.js'
 
 const router = express.Router();
 
 router.post('/', registrar)
 
 router.get('/perfil', perfil)
+// express permette params dinamico :
+router.get('/confirmar/:token', confirmar)
 
 
 
